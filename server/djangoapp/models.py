ed46d7ea84e75b63626f7faf5c03abd100287c53
Kildall <pebustos.135@gmail.com>
2022-08-07 17:59:44 -0300

    Moved models to admin.py and finished them

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -3,16 +3,7 @@ from django.utils.timezone import now
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-
-
-
-# Create your models here.
-
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+from django.contrib import admin
 
 class LoginForm(forms.Form):
     username = forms.CharField(label="Username", max_length=50, widget=forms.TextInput(attrs={'placeholder': 'Username'}))
@@ -26,15 +17,6 @@ class RegisterForm(UserCreationForm):
         model = User
         fields = ["username", "password1", "password2", "first_name", "last_name"]
 
-
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
-# - Name
-# - Dealer id, used to refer a dealer created in cloudant database
-# - Type (CharField with a choices argument to provide limited choices such as Sedan, SUV, WAGON, etc.)
-# - Year (DateField)
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
 class CarMake(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=100)
@@ -50,7 +32,7 @@ class CarModel(models.Model):
         Sedan = 'Sedan'
         Wagon = 'Wagon'
         Compact = 'Compact'
-
+    
     make = models.ForeignKey(to=CarMake, on_delete=models.DO_NOTHING)
     dealer_id = models.IntegerField()
     name = models.CharField(max_length=50)
@@ -59,6 +41,11 @@ class CarModel(models.Model):
 
     def __str__(self):
         return self.name
+
+
+
+
+
 
 
 
